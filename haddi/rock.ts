bcb7999cc476e7f396baf1127e36b448da796e0b
Muhammad Ahmad <ahmad321ali007@gmail.com>
2024-07-03 15:41:43 +0500

    Add files via upload

---
+++ haddi/rock.ts
@@ -0,0 +1,76 @@
+console.group("AHMAD");
+let myname = "ROCKSTAR";
+console.log(myname)
+let num1 = 2+5;
+let x = 2
+ x += 5; // 7
+let y = 5-2// 3
+ y -= 2// 1
+console.log(y);
+// let z = x + y
+console.log(x);
+
+console.log(num1)
+let z = 6 * 6
+z -= 2// 34
+console.log(z)
+let d = 2/4
+d -= 2// 0
+console.log(d)
+
+// EXPONENTATION
+let q = 2 ** 4 //16
+console.log(q)
+ 
+// MODULUS
+let m = 5 % 2 // 1 reminder 
+console.log(m)
+
+ x = 5
+ y = 6
+ z = x + y
+console.log(z)
+
+let r = 6 * 6
+r -= 2 // 34
+console.log(r)
+console.log("ROCKSTAR IS MY PERSONAL TAG");
+ 
+// MODULUS
+
+let o = 5 % 2 // 1 
+console.log(o);
+
+// UNARY OPERTORS ++ , --
+// UNARY OPERTORS ARE INCREMENT AND DECREMENT
+
+let f = 8;
+f++
+console.log(f);
+
+let h = 9
+h++ //1 //10
+h-- //1 //9
+console.log(h)
+
+let t = 8
+let j = 5
+let a = t++ + t-- + j++ + --t - --j
+console.log(8+1 + 8-1 + 5+1 + 8-1 - 5-1 );
+console.log(9 + 7 + 6 + 7 - 4);
+
+let p = 8
+p++ //9
+console.log(p++);
+console.log(p);
+
+// COMBINING OPERATORS BODMAS 
+
+let i = 2*5-4+3/9+(3+6);
+console.log(2*5-4+3/9+6);
+console.log(2*5-4+3+6);
+console.log(10-4+3+6);
+console.log(10-13);
+let weight = 50;
+console.log(weight);
+
